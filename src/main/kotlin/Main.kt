d3dfb3fd95dd8ecaa351cbb24f96e20b3b4a1da4
Kristina <volosnikova-k@yandex.ru>
2022-10-11 22:23:23 +0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val likes = 1
+    val person = "человеку"
+    val people = "людям"
+
+    if (likes % 10 == 1 && likes !=11){
+        println ("Понравилось $likes $person")
+    } else {
+        println("Понравилось $likes $people")
+    }
+}
